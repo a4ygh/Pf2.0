812e9baf9d15f19266d12d733c4cf0f7666457ae
a4ygh <153000429+a4ygh@users.noreply.github.com>
2024-03-18 17:33:03 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var password = "qT23P5<3/bPt";
-(function passcodeprotect() {
-   var passcode = prompt("Passwort Eingeben!");
-   while (passcode !== password) {
-      alert("Passwort ist Falsch!");
-      return passcodeprotect();
-   }
-}());
-alert('Passwort ist Richtig!');
