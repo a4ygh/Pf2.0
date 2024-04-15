ee5d47e7a6f889fbe2c2c1bd995df19b6270191c
a4ygh <153000429+a4ygh@users.noreply.github.com>
2024-04-15 10:28:30 +0200

    Delete Andere Seiten (Projekten) directory

--- Andere Seiten (Projekten)/projekt0.5.js
+++
@@ -1,9 +0,0 @@
-//remove this comment to activate funktion: Event = Loading screen// window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-
-    loader.classList.add("loader-hidden");
-
-    loader.addEventListener("transitioned", () => {
-        document.body.removeChild("loader");
-    })
-})
